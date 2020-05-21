551428c457398268654ffbf2696e8ea0dc17bce9
Phips04 <65680501+Phips04@users.noreply.github.com>
2020-05-21 09:58:08 +0200

    Add files via upload

---
+++ Pair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pair
+{
+    public sealed class Pair<T, U>
+    {
+        public static Pair<T, U> MakePair(T first, U second)
+        {
+            Pair<T, U> ReturnValue = new Pair<T, U>
+            {
+                First = first,
+                Second = second
+            };
+
+            return ReturnValue;
+        }
+
+        public override string ToString() => First.ToString() + " - " + Second.ToString();
+
+        public T First { get; set; }
+        public U Second { get; set; }
+    }
+}
